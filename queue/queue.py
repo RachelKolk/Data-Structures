32614661f1ae4a680c442262f4d660503fde196b
RachelKolk <rach.m.obrien@gmail.com>
2019-05-13 20:10:40 -0400

    queue.py coded and tests passing

--- queue/queue.py
+++ queue/queue.py
@@ -3,13 +3,19 @@ class Queue:
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.storage = []
 
   def enqueue(self, item):
-    pass
+    #insert method to add item to queue
+    if item not in self.storage:
+      self.storage.insert(0, item)
+      return True
+    return False
   
   def dequeue(self):
-    pass
+    #using pop to remove item from queue
+    if len(self.storage) > 0:
+      return self.storage.pop()
 
   def len(self):
-    pass
+    return len(self.storage)
