ff70751ac1609a453e7a25e8c6eabe9924c21df5
RachelKolk <rach.m.obrien@gmail.com>
2019-05-16 14:51:36 -0400

    max_heap.py all functions coded and tests passing

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,6 +1,6 @@
 class Heap:
   def __init__(self):
-    self.storage = [0]
+    self.storage = []
     
 
   def insert(self, value):
@@ -11,15 +11,15 @@ class Heap:
 
   def delete(self):
     # if the heap has two or more values
-    if len(self.storage) > 2:
+    if len(self.storage) > 1:
       # we swap the first and last values of the heap
-      self._swap(1, len(self.storage) - 1)
+      self._swap(0, len(self.storage) - 1)
       # we pop the max value off the heap and store it in a max variable
       max = self.storage.pop()
       #and then we sift the swapped item down to where it belongs
-      self._sift_down(1)
+      self._sift_down(0)
     #if there is only one variable in the heap
-    elif len(self.storage) == 2:
+    elif len(self.storage) == 1:
       # we save it as the max variable so we can return it as the largest item
       max = self.storage.pop()
     else: 
@@ -29,14 +29,14 @@ class Heap:
 
   def get_max(self):
     # make sure that our heap contains at least one item...
-    if self.storage[1]:
+    if self.storage[0]:
       #if there is it will return the top item because that will be the largest
-      return self.storage[1]
+      return self.storage[0]
     else:
       return False
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   # created this just to make it easier to swap nodes in the functions
   def _swap(self, i, k):
@@ -44,9 +44,9 @@ class Heap:
 
   def _bubble_up(self, index):
     # find the parent index of the new node index
-    parent = index // 2
+    parent = (index - 1) // 2
     # if the value of the new node is less than its parent...
-    if index <= 1:
+    if index <= 0:
       #it will already be in the right order and we don't have to do anything
       return
     # if the value of the index is greater than its parent...
@@ -59,8 +59,8 @@ class Heap:
 
   def _sift_down(self, index):
     # start by defining the index formulas assigned to each side
-    left = index * 2
-    right = index * 2 + 1
+    left = index * 2 + 1
+    right = index * 2 + 2
     largest = index
     # we compare the index value to both the left and right to see which is the largest
     if len(self.storage) > left and self.storage[largest] < self.storage[left]:
